cc2c9b8c2aa6310f5b677dd7c3646d3139b806db
ELENA <battilena@gmail.com>
2022-10-13 20:39:39 +0300

    function

---
+++ l.py/function.py
@@ -0,0 +1,12 @@
+#function - фрагмент программы используемый многократно
+
+def f(x):
+    if x == 1:
+        return 'целое'
+    elif x == 2.3:
+        return 23
+    else:
+        return        
+arg =2
+print(f(arg))
+print(type(f(arg)))        
